d40b727f0db49fee0396004226d7c57cab3ce085
Mirco Bozzolini <mirco.bozzolini@lantechlongwave.it>
2024-10-02 16:43:35 +0200

    docs: removed comment

--- example/simple_example/main.tf
+++ example/simple_example/main.tf
@@ -8,5 +8,3 @@ module "gitops_updater" {
   # codecommit_repo_arn = "arn:aws:codecommit:eu-west-1:687331130220:ll-k8s-lab01-apps"
   codecommit_repo_name = "ll-k8s-lab01-apps"
 }
-
-# aws codecommit get-repository --repository-name ll-k8s-lab01-apps --query 'repositoryMetadata.Arn' --output text --profile innovation
