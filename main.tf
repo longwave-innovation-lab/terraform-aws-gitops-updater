e02366e768343a9dca1c4f99fc84d24d158ec078
Mirco Bozzolini <mirco.bozzolini@lantechlongwave.it>
2025-01-30 17:28:03 +0100

    fix: added default tags with Service Project GitopsUpdater

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ data "aws_iam_policy_document" "assume_role_codebuild" {
 resource "aws_iam_role" "codebuild_role" {
   name_prefix        = substr("${var.codebuild_project_name}", 0, 38)
   assume_role_policy = data.aws_iam_policy_document.assume_role_codebuild.json
+  tags = var.tags
 }
 
 # Needed to lookup if images was already present or not
@@ -132,6 +133,7 @@ resource "aws_codebuild_project" "cb_project" {
   cache {
     type = "NO_CACHE"
   }
+  tags = var.tags
 }
 
 # aws codebuild batch-get-projects --names TepK8sAppsGitOpsSynchEF1D90-vPWKPSV49Jq2 --profile tep
@@ -154,6 +156,7 @@ resource "aws_iam_role" "lambda_function_role" {
       }
     ]
   })
+  tags = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
@@ -213,6 +216,7 @@ resource "aws_lambda_function" "codebuild_triggerer" {
       "CODEBUILD_PROJECT_NAME" = aws_codebuild_project.cb_project.name
     }
   }
+  tags = var.tags
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
@@ -239,6 +243,7 @@ resource "aws_cloudwatch_event_rule" "ecr_image_push" {
       repository-name = var.ecr_registry_triggers
     }
   })
+  tags = var.tags
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
@@ -258,6 +263,7 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
 
 resource "aws_sqs_queue" "evnt_rule_target_dlq" {
   name = "${var.event_rule_target_id}-dlq"
+  tags = var.tags
 }
 
 #endregion EventRule
